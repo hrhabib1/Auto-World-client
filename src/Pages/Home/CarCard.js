397a7ab8e665416ce2c6c5b048165f69d1330934
hrhabib1 <sayefhabib123@gmail.com>
2022-11-25 02:33:05 +0600

    home page done

--- src/Pages/Home/CarCard.js
+++ src/Pages/Home/CarCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {AiOutlineStar} from 'react-icons/ai'
 
 const CarCard = ({ car }) => {
-    const {carName, date, imgURL } = car;
+    const {carName, date, imgURL, mileage, location, engine, transmission, price} = car;
     return (
         <div className="card bg-white m-2 px-6 pt-6">
             <div className='flex justify-between items-center'>
@@ -13,13 +13,22 @@ const CarCard = ({ car }) => {
                <button className='bg-red-200 p-2'><AiOutlineStar/></button>
             </div>
             <figure className="mt-4">
-                <img src={imgURL} alt="carImage" className="rounded-xl h-72" />
+                <img src={imgURL} alt="carImage" className="rounded-xl h-60" />
             </figure>
-            <div className="card-body items-center text-center">
-                
-                <div className="card-actions">
-                    <button className="btn btn-primary">Buy Now</button>
+            <div className="my-3">
+                <div className='flex justify-between'>
+                    <p><span className='text-primary'>Mileage: </span>{mileage}</p>
+                    <p><span className='text-primary'>Location: </span>{location}</p>
                 </div>
+                <div className='flex justify-between'>
+                    <p><span className='text-primary'>Engine: </span>{engine}</p>
+                    <p><span className='text-primary'>Transmission: </span>{transmission}</p>
+                </div>
+                <div className='flex justify-between my-3'>
+                    <p className='text-2xl font-bold'>{price}</p>
+                    <button className='bg-red-500 py-1 px-6 text-white'>More details</button>
+                </div>
+               
             </div>
         </div>
     );
