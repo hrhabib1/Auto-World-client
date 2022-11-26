8a7bcc58634cc7e7a10580033c9c4edd01f81d6d
hrhabib1 <sayefhabib123@gmail.com>
2022-11-26 14:18:27 +0600

    done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-2 m-2'>
             {
                         cars.slice(0).reverse().map(car => <CarCard
-                            key={car.carName}
+                            key={car._id}
                             car={car}
                         ></CarCard>)
                     }
